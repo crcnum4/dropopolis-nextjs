fb77c66910d3e35b058767f40fc00a1ccac660a4
qzpimae <yoshua.theg@gmail.com>
2022-08-12 09:00:48 -0400

    remove Image component temp for test hosting (created build fail on staged-production)

--- components/Home/Footer/AboutModule.tsx
+++ components/Home/Footer/AboutModule.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 import Image from "next/image";
-import logo from '../../../public/assets/logo.png'
+const logo = '/assets/logo.png'
 
 type AboutModuleProps = {
   style?: React.CSSProperties
@@ -9,7 +9,7 @@ type AboutModuleProps = {
 const AboutModule: FC<AboutModuleProps> = (props) => {
   return (
     <div style={{...props.style}} className={props.className || "max-w-screen-sm text-left mb-8 mx-10 my-6"}>
-        <Image src={logo} alt='Dropopolis' className='h-10 w-fit' />
+        <img src={logo} alt='Dropopolis' className='h-10 w-fit' />
         <p className='mt-3'>A real city in the Metaverse, built by & for YOU!</p>
     </div>
   )
