d99a964db8c5fbe6365493a0020c5fbb9cc2388c
GabrielQZ <gabrielquinn.photography@gmail.com>
2022-08-16 19:52:57 -0400

    add bordercard and update aboutModule

--- components/Home/Footer/AboutModule.tsx
+++ components/Home/Footer/AboutModule.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import Image from "next/image";
-import logo from '../../../public/assets/logo.png'
+import router from 'next/router';
+const logo = '/assets/logo.png'
 
 type AboutModuleProps = {
   style?: React.CSSProperties
@@ -9,7 +10,9 @@ type AboutModuleProps = {
 const AboutModule: FC<AboutModuleProps> = (props) => {
   return (
     <div style={{...props.style}} className={props.className || "max-w-screen-sm text-left mb-8 mx-10 my-6"}>
-        <Image src={logo} alt='Dropopolis' className='h-10 w-fit' />
+        <div className="relative w-fill h-10  cursor-pointer " onClick={() => router.push('/')}>
+            <Image src={logo} alt='Dropopolis' layout='fill' objectFit='contain'></Image>
+        </div>
         <p className='mt-3'>A real city in the Metaverse, built by & for YOU!</p>
     </div>
   )
