032e8dc84bec5940a8dd4236a2ca9a5c81adf30c
GabrielQZ <gabrielquinn.photography@gmail.com>
2022-08-16 16:35:53 -0400

    remove prefilled form data

--- pages/upload/index.tsx
+++ pages/upload/index.tsx
@@ -26,11 +26,11 @@ const UploadPage : NextPage = () => {
     ...initialArtNftUploadQuery,
 
     //UNCOMMENT THIS TO TEST WITH A PRE-FILLED FORM
-    name: "Test NFT 1",
-    symbol: "TEST",
-    description: "desc",
-    externalUrl: "dropopolis.com",
-    resaleFee: "5",
+    // name: "Test NFT 1",
+    // symbol: "TEST",
+    // description: "desc",
+    // externalUrl: "dropopolis.com",
+    // resaleFee: "5",
   })
 
   const [error, setError] = useState<ArtNftUploadErrors>({
